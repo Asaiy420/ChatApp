a1cfdb48b1ae8c08bbb85473e7a01a229a2b50a8
Asaiy420 <kcaryan05@gmail.com>
2025-04-10 14:16:58 +0545

    added controllers and database

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("signup route page")
+}
+
+export const login = (req, res) => {
+    res.send("signup route page")
+}
+
+export const logout = (req, res) => {
+    res.send("signup route page")
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,12 @@
 import express from "express"
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("signup route page")
-})
+router.post("/signup", signup)
 
-router.get("/login", (req, res) => {
-    res.send("login route page")
-})
+router.post("/login", login)
 
-router.get("/logout", (req, res) => {
-    res.send("logout route page")
-}) 
+router.post("/logout", logout) 
 
 export default router;
