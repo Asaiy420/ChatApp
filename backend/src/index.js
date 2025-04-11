468e272567d406183ea0fed3840a5ed1b3aacbd5
Asaiy420 <kcaryan05@gmail.com>
2025-04-11 12:37:39 +0545

    added axios and cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,21 +2,28 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
-import messageRoutes from "../src/routes/message.route.js"
+import cookieParser from "cookie-parser";
+import messageRoutes from "../src/routes/message.route.js";
+import cors from "cors";
 
-
-dotenv.config()
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;  
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json()) // body parser to decode json value from body requests
-app.use(cookieParser()) // cookie parser to decode values from the jwt cookie for validation
+app.use(express.json()); // body parser to decode json value from body requests
+app.use(cookieParser()); // cookie parser to decode values from the jwt cookie for validation
+// cors allows frontend to interact with backend api
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, //allows cookies to be sent
+  })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
-  connectDB()
+  connectDB();
 });
