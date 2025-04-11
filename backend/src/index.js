962675008a4043fecb770446e06c3985fd78e427
Asaiy420 <kcaryan05@gmail.com>
2025-04-11 08:10:53 +0545

    added cookieparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,15 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 
 
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3000;  
 
-app.use(express.json()) // body parser to extract json value from body requests
-
+app.use(express.json()) // body parser to decode json value from body requests
+app.use(cookieParser()) // cookie parser to decode values from the jwt cookie for validation
 
 app.use("/api/auth", authRoutes)
 
