310e30e0638750e8ba805bb81d79acc7193c7e94
Asaiy420 <kcaryan05@gmail.com>
2025-04-12 17:13:19 +0545

    added home page and fixed typos

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,36 @@
+import {create} from "zustand"
+import toast from "react-hot-toast"
+import {axiosInstance} from "../lib/axios.js"
+
+export const useChatStore = create ((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    
+    
+    getUser: async () => {
+        set({isUsersLoading: true})
+        try {
+            const res = await axiosInstance.get("/messages/users")
+            set({users: res.data})
+        } catch (error) {
+            toast.error(error.response.data.messages)
+        }finally{
+            set({isUsersLoading: false})
+        }
+    },
+    getMessages: async (userId) => {
+        set({isMessagesLoading: true});
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({messages: res.data})
+        } catch (error) {
+            toast.error(error.response.data.messages)
+        }finally{
+            set({isMessagesLoading: false})
+        }
+    },
+    setSelectedUser: async (selectedUser) => set({selectedUser})
+}))
