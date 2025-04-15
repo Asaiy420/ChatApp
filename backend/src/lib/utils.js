f3209d512d7f31c669a60f3f6de3cc284463f1a6
Asaiy420 <kcaryan05@gmail.com>
2025-04-15 12:12:55 +0545

    fixed typo

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,7 +4,7 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  //* Send jwt in form of cookie
+  //* Send jwt in form of cookies
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7d in milliseconds.
